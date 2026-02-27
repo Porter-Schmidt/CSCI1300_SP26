0cb0ea80026e7a35f48a55003bbfe7ef07633518
Porter Schmidt <posc5487@colorado.edu>
2026-02-27 15:36:44 -0700

    work

--- Week7/FridayWarmUp/Array.cpp
+++ Week7/FridayWarmUp/Array.cpp
@@ -6,9 +6,14 @@ int main() {
     int numbers[SIZE] = {3, 8, 12, 5, 9, 1};
 
     // TODO: print numbers in original order​
-
+    for (int i = 0; i < SIZE; i++) {
+        cout << numbers[i] << endl;
+    }
+    cout << endl;
     // TODO: print numbers in reverse order​
-
+    for (int i = SIZE - 1; i >= 0; i--) {
+        cout << numbers[i] << endl;
+    }
     return 0;
 }
 
